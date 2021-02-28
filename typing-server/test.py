beba9933ecb762c5b94c505fa622e82f2a2b616a
aditya02acharya <aditya02acharya@gmail.com>
2021-02-28 10:30:52 +0200

    updating packages in requirements.txt

--- typing-server/test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from api import init_log_config
-from tests.api.aiagent.device_test import DeviceTestCase
-
-if __name__ == "__main__":
-    init_log_config()
-    DeviceTestCase()
-    unittest.main()
